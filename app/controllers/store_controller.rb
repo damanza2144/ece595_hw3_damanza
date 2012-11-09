66ad9e722f2196416aec05128bfe17fa64a5b989
damanza2144 <damanza@unm.edu>
2012-11-08 21:24:51 -0700

    no real changes

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,7 @@
 class StoreController < ApplicationController
   
   def index
-	#@current_page = params[:page]
+	
 	@products = Product.paginate :page => params[:page], :order => 'title asc', :per_page => 3
 	@cart = current_cart
   end
